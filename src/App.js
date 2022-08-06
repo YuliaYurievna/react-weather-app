23ec9e376f6acb0993dfbddff504472ae4059c19
YuliaYurievna <104911480+YuliaYurievna@users.noreply.github.com>
2022-08-06 19:41:04 +0300

    Added Bootstrapt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      <p>
+        <a href="https://github.com/YuliaYurievna/react-weather-app" rel="noreferrer" target="_blank">Open-source code</a>, by Yuliia Poddiacha
+      </p>
+    </footer>
+  </div>
+  );
+
 }
 
 
