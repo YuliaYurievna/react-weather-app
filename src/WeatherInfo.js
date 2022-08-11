5c93a9cd93a956282dc5cc022d7655379509bd1a
YuliaYurievna <104911480+YuliaYurievna@users.noreply.github.com>
2022-08-11 16:04:33 +0300

    Deleted unnecessery markup

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,14 +7,14 @@ import WeatherTemperature from './WeatherTemperature';
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+      <h1 className='mb-2'>{props.data.city}</h1>
       <ul>
         <li>
           <FormattedDate date={props.data.date} />
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
+      <div className="row mt-2">
         <div className="col-6">
 
           <div className="d-flex">
